0456dc3d67f5e781b9d4f5df6b63d08f20d951eb
harphield <harphield@gmail.com>
2020-03-09 13:41:52 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "~bootstrap";
+@import "~bootstrap-vue";
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <bar-chart />
+
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import BarChart from "@/lib/charts/BarChart";
+
+  export default {
+    name: "About",
+    components: {
+      BarChart
+    }
+  };
+</script>
