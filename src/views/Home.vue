4dee0ebef61711b1eecef6ccf8342bd19c2c8514
Matus Marcin <matus.marcin@gmail.com>
2020-03-09 18:19:58 +0100

    Simple scan from DynamoDB

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,16 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { listReports } from "@/lib/data/db.ts";
 
 export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  async mounted() {
+    const reports = await listReports();
+    console.log(reports);
   }
 };
 </script>
