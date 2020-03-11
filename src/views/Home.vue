47920c0373c68ad85effab1d78e1c5511122c959
Matus Marcin <matus.marcin@gmail.com>
2020-03-11 11:19:40 +0100

    Added error_rate_admin and background_jobs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="row about">
     <error-rate-chart reportName="error_rate_martinus" />
+    <error-rate-chart reportName="error_rate_admin" />
+    <error-rate-chart reportName="error_rate_background_jobs" />
     <error-rate-chart reportName="response_time_pda" />
     <div class="col-sm">
       <test-line-chart-container />
